afe6d7442281b7c279ddc1594c5cb6c42bc41731
RANA ASAD ALI <128570840+rana123RANA123@users.noreply.github.com>
2024-10-23 11:53:11 +0500

    Update index.js

--- src/validations/index.js
+++ src/validations/index.js
@@ -1,5 +1,5 @@
 module.exports.authValidation = require('./auth.validation');
 module.exports.userValidation = require('./user.validation');
 module.exports.boardValidation = require('./boards.validation');
-module.exports.workspaceValidation = require('./workspace.validation');
+module.exports.workspaceValidation = require('./workSpace.validation');
 module.exports.boardInvitationValidation = require('./boardInvitation.validation');
