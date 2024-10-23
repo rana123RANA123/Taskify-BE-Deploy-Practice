dd097a810cf290d823191c1bc8654b241d8810de
RANA ASAD ALI <128570840+rana123RANA123@users.noreply.github.com>
2024-10-23 11:56:53 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -56,8 +56,6 @@ if (config.env === 'production') {
 // v1 api routes
 app.use('/v1', routes);
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
-
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
