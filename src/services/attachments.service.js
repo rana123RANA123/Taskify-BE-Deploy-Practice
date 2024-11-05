552011ce774e58b8faa4221c8075fecf9f4b3b79
RANA ASAD ALI <128570840+rana123RANA123@users.noreply.github.com>
2024-11-05 18:53:50 +0500

    Update attachments.service.js

--- src/services/attachments.service.js
+++ src/services/attachments.service.js
@@ -1,21 +1,33 @@
 const Attachment = require('../models/attachments.modal');
 
 const createAttachment = async (data, file) => {
+  // Initialize the attachment data with common fields
   const attachmentData = {
     type: data.type,
     ticketId: data.ticketId,
     optionalText: data.optionalText,
+    imageName: data.imageName,
+    url: data.url,
   };
 
+  // Handle different types of attachments
   if (data.type === 'image' && file) {
+    // If the type is 'image' and a file is provided, set the imageName to the file's name
     attachmentData.imageName = file.filename;
   } else if (data.type === 'link') {
+    // If the type is 'link', set the link property
     attachmentData.link = data.link;
   }
 
+  // Create a new attachment instance using the prepared data
   const attachment = new Attachment(attachmentData);
+
+  // Save the attachment to the database and return the result
   return await attachment.save();
 };
+
+module.exports = createAttachment;
+
 
 const getAttachments = async () => {
   const attachments = await Attachment.find();
